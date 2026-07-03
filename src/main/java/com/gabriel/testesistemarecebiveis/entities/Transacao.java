1bd616390d42aea4b926b8e3fc8808488159082d
Gabriel Gomes <gabrielsogom@gmail.com>
2026-07-03 10:30:53 -0300

    test: ajustando teste para cocompor o cenário

--- src/main/java/com/gabriel/testesistemarecebiveis/entities/Transacao.java
+++ src/main/java/com/gabriel/testesistemarecebiveis/entities/Transacao.java
@@ -1,21 +1,7 @@
 package com.gabriel.testesistemarecebiveis.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
-import jakarta.persistence.Version;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
